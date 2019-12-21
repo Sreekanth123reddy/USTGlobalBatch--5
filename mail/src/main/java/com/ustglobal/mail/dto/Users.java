8fb190917b602f2854786a5d28ebf6a9ac09bedd
Sreekanth123reddy <sreekanthcsc1997@gmail.com>
2019-12-21 11:28:03 +0545

    project

---
+++ mail/src/main/java/com/ustglobal/mail/dto/Users.java
@@ -0,0 +1,48 @@
+package com.ustglobal.mail.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_info")
+public class Users {
+	@Id
+	@Column
+	@GeneratedValue
+	private int id;
+	@Column
+	private String user_name;
+	@Column(unique = true)
+	private String email;
+	@Column	
+	private String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
